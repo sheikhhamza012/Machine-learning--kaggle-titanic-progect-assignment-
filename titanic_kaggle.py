2e3f6f8a3e1a045c96557a4a74c6cf138e889d61
Sheikh hamza <sheikhhamza012@gmail.com>
2019-10-25 18:56:04 +0500

    project initialised

---
+++ titanic_kaggle.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib as plt
+import pandas as pd 
+test = pd.read_csv('/home/mehroz/Desktop/ml/code1/input/test.csv', encoding='utf8', engine='python')
+train = pd.read_csv('/home/mehroz/Desktop/ml/code1/input/train.csv', encoding='utf8', engine='python')
+print train
