996a9d86fa4001336484b9f81e19d1caf54e8ca6
Sheikh hamza <sheikhhamza012@gmail.com>
2019-10-27 20:05:45 +0500

    Data relations analysed

--- titanic_kaggle.py
+++ titanic_kaggle.py
@@ -4,8 +4,8 @@ import pandas as pd
 # import seaborn as sb
 #%matplotlib inline
 # sb.set()
-test =pd.read_csv('/home/mehroz/Desktop/ml/code1/input/test.csv', encoding='utf8', engine='python')
-train = pd.read_csv('/home/mehroz/Desktop/ml/code1/input/train.csv', encoding='utf8', engine='python')
-
+test =pd.read_csv('C:\Users\sheik\Desktop\ML\kaggle\Machine-learning--kaggle-titanic-progect-assignment-\input\test.csv', encoding='utf8', engine='python')
+train = pd.read_csv('C:\Users\sheik\Desktop\ML\kaggle\Machine-learning--kaggle-titanic-progect-assignment-\input\train.csv', encoding='utf8', engine='python')
+print test
 
 # sb.countplot(x='Survived',y= train,)
